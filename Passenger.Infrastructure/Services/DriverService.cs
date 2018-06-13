891aa0e45d1dff887da5c9905aed34300f9058fa
Xantiros <xantiros@gmail.com>
2018-06-13 12:30:02 +0200

    Revert "Revert "Testy, FluentAssertion, Moq zainstalowane""
    This reverts commit 1dba3f5f9d69de8e48d5cf688884320717239680.

--- Passenger.Infrastructure/Services/DriverService.cs
+++ Passenger.Infrastructure/Services/DriverService.cs
@@ -12,9 +12,10 @@ namespace Passenger.Infrastructure.Services
         private readonly IDriverRepository _driverRepository;
         private readonly IMapper _mapper;
 
-        public DriverService(IDriverRepository driverRepository)
+        public DriverService(IDriverRepository driverRepository, IMapper mapper)
         {
             _driverRepository = driverRepository;
+            _mapper = mapper;
         }
 
         public async Task<DriverDto> GetAsync(Guid userId)
