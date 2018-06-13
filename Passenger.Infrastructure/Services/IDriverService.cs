9d03572febdc9da3fb425d17100e90c0afbfd6e0
Xantiros <xantiros@gmail.com>
2018-06-13 11:12:43 +0200

    Change async

--- Passenger.Infrastructure/Services/IDriverService.cs
+++ Passenger.Infrastructure/Services/IDriverService.cs
@@ -1,12 +1,11 @@
 ﻿using Passenger.Infrastructure.DTO;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Passenger.Infrastructure.Services
 {
     public interface IDriverService
     {
-        DriverDto Get(Guid userId);
+        Task<DriverDto> GetAsync(Guid userId);
     }
 }
