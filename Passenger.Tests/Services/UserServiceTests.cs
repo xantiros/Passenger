2ecfa9f72c125805f11ef4e3fe3ec0b23486dbc9
Xantiros <xantiros@gmail.com>
2018-06-13 16:59:13 +0200

    New tests

--- Passenger.Tests/Services/UserServiceTests.cs
+++ Passenger.Tests/Services/UserServiceTests.cs
@@ -6,6 +6,7 @@ using Passenger.Core.Repositories;
 using Moq; //moq - nuget zainstalowany 
 using AutoMapper;
 using Passenger.Core.Domain;
+using System;
 
 namespace Passenger.Tests.Services
 {
@@ -22,6 +23,38 @@ namespace Passenger.Tests.Services
 
             userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
 
+        }
+
+        [Fact]
+        public async Task when_calling_get_async_and_user_exists_it_should_invoke_user_repository_get_async()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+            await userService.GetAsync("user1@email.com");
+
+            var user = new User("user1@email.com", "user1", "secret", "salt");
+
+            userRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>()))
+                              .ReturnsAsync(user);
+
+            userRepositoryMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task when_calling_get_async_and_user_does_not_exist_it_should_invoke_user_repository_get_async()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+            await userService.GetAsync("user@email.com");
+
+            userRepositoryMock.Setup(x => x.GetAsync("user@email.com"))
+                              .ReturnsAsync(() => null);
+
+            userRepositoryMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once());
         }
     }
 }
